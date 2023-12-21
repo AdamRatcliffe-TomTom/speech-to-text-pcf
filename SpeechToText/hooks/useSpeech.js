19f44af8b84a1e9d8de52fc38837e4da6e663b1a
Adam Ratcliffe <adam.ratcliffe@tomtom.com>
2023-12-20 16:35:28 -0800

    Work around a bug in the web-speech-cognitive-services library.

--- SpeechToText/hooks/useSpeech.js
+++ SpeechToText/hooks/useSpeech.js
@@ -8,6 +8,8 @@ import {
 
 let SpeechRecognition;
 let recognition;
+let stopping = false;
+let haveFinalResult = false;
 
 const useSpeech = () => {
   const [text, setText] = useState("");
@@ -38,17 +40,31 @@ const useSpeech = () => {
     recognition.start();
 
     recognition.addEventListener("result", (event) => {
-      const transcript = Array.from(event.results)
+      const resultsArray = Array.from(event.results);
+      const transcript = resultsArray
         .map((result) => result[0].transcript)
         .join(" ");
+      haveFinalResult = resultsArray.at(-1).isFinal;
+
       setText(transcript);
+
+      if (stopping && haveFinalResult) {
+        recognition?.abort();
+        stopping = false;
+      }
     });
   };
 
+  // Need to work around a bug in the web-speech-cognitive-services library
+  // where calling stop() fails to stop recording when continuously listening
+  // https://github.com/compulim/web-speech-cognitive-services/issues/166
   const stopRecognition = () => {
-    // Should call stop() but a bug with the web-speech-cognitive-services
-    // library requires abort() instead
-    recognition?.abort();
+    if (haveFinalResult) {
+      recognition?.abort();
+      stopping = false;
+    } else {
+      stopping = true;
+    }
   };
 
   return {
